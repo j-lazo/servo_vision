174fc4943068eee16ba6f9449e62a3fe831ce3dd
j-lazo <j.org.ls@ciencias.unam.mx>
2021-05-11 19:44:52 +0200

    data management functions

---
+++ scripts/general_functions/data_managament.py
@@ -0,0 +1,88 @@
+from glob import glob
+import numpy as np
+import cv2
+import tensorflow as tf
+
+def load_data(path):
+    print(path)
+    path_images = ''.join([path, 'image/', image_modality, "/*"])
+    path_labels = ''.join([path, "label/*"])
+    images = sorted(glob(path_images))
+    masks = sorted(glob(path_labels))
+    total_size_images = len(images)
+    total_size_labels = len(masks)
+    print('total size images:', total_size_images, path_images)
+    print('total size labels:', total_size_labels, path_labels)
+    return (images, masks)
+
+def load_data_only_imgs(path):
+    print(path)
+    path_images = ''.join([path, "/*"])
+    images = sorted(glob(path_images))
+    total_size_images = len(images)
+    print('total size images:', total_size_images, path_images)
+    return (images, images)
+
+
+def read_image_test(path):
+    x = cv2.imread(path, cv2.IMREAD_COLOR)
+    x = cv2.resize(x, (256, 256))
+    x = x / 255.0
+    return x
+
+
+def read_mask_test(path):
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    x = cv2.resize(x, (256, 256))
+    x = np.expand_dims(x, axis=-1)
+    return x
+
+
+def read_image(path):
+
+    path = path.decode()
+    x = cv2.imread(path, 1)
+    x = cv2.resize(x, (256, 256))
+    x = x/255.0
+    return x
+
+
+def read_mask(path):
+    path = path.decode()
+    x = cv2.imread(path)
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+    x = cv2.resize(x, (256, 256))
+    x = x/255.0
+    x = np.expand_dims(x, axis=-1)
+    return x
+
+
+def tf_parse(x, y):
+    def _parse(x, y):
+        x = read_image(x)
+        y = read_mask(y)
+        return x, y
+
+    x, y = tf.numpy_function(_parse, [x, y], [tf.float64, tf.float64])
+    x.set_shape([256, 256, 3])
+    y.set_shape([256, 256, 1])
+    return x, y
+
+
+def tf_dataset(x, y, batch=8):
+    dataset = tf.data.Dataset.from_tensor_slices((x, y))
+    dataset = dataset.map(tf_parse)
+    dataset = dataset.batch(batch)
+    dataset = dataset.repeat()
+    return dataset
+
+
+def iou(y_true, y_pred, smooth=1e-15):
+    def f(y_true, y_pred):
+        intersection = (y_true * y_pred).sum()
+        union = y_true.sum() + y_pred.sum() - intersection
+        x = (intersection + smooth) / (union + smooth)
+        x = x.astype(np.float32)
+        return x
+
+    return tf.numpy_function(f, [y_true, y_pred], tf.float32)
