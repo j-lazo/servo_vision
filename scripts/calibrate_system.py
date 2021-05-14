1ffcc70b67961fed9a4bdfdec902086572e7768f
j-lazo <j.org.ls@ciencias.unam.mx>
2021-05-14 12:40:03 +0200

    added test actuators function

--- scripts/calibrate_system.py
+++ scripts/calibrate_system.py
@@ -75,6 +75,62 @@ def test_camera():
     # De-allocate any associated memory usage
     cv2.destroyAllWindows()
 
+def test_all_systems_together():
+
+    # Settings for the em tracking sensor
+    SETTINGS={
+        "tracker type": "aurora",
+        "romfiles": [os.getcwd() + "/scripts/em_tracking/080082.rom"],
+    }
+    TRACKER = NDITracker(SETTINGS)
+    TRACKER.start_tracking()
+    # Setting up the video recording settings for the camera
+    cap = cv2.VideoCapture(0)
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (300, 300))
+
+    while True:
+        port_handles, timestamps, framenumbers, tracking, quality = TRACKER.get_frame()
+        if not ret:
+            print('Cannot recieve frame (stream end?). Exiting')
+            break
+
+        for i, t in enumerate(tracking):
+            print(i)
+            x = t[0][3]
+            y = t[1][3]
+            z = t[2][3]
+            print(x, y, z)
+
+        # reads frames from a camera
+        # ret checks return at each frame
+        ret, frame = cap.read()
+        # read serial to obtain current states of actuation joint variables
+        motor_encoders = pysr.request_encoder_value()  # motor_encoders contains is a list contain the current encoder value
+        print(motor_encoders)
+        # frame is converted to hsv
+        resized = cv2.resize(frame, (500, 300))
+        # The original input frame is shown in the window
+        cv2.imshow('Output Camera', resized)
+
+        # Wait for 'a' key to stop the program
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+
+        # send serial for new set points for PID controller
+
+
+    # stop tracking
+    TRACKER.stop_tracking()
+    TRACKER.close()
+    # Close the window / Release webcam
+    cap.release()
+    # After we release our webcam, we also release the output
+    out.release()
+    # De-allocate any associated memory usage
+    cv2.destroyAllWindows()
 
 def acquire_data_camera_calibration(save_dir):
 
@@ -148,9 +204,9 @@ if __name__ == "__main__":
         test_camera()
     elif args.command == 'test_sensor':
         test_sensor()
+    elif args.command == "test_actuators":
+        test_actuators()
     elif args.command == "acquire_data_camera_calibration":
-        acquire_data_camera_calibration(args.save_dir)
-    elif args.command == "acquire_data_hande_eye_calibration":
         acquire_data_camera_calibration(args.save_dir)
     elif args.command == 'hand_eye_calibration':
         acquire_data_camera_calibration(args.save_dir)
