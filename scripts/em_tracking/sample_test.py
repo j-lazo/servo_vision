515ba123e74ce8cfd2338b09735a76f2b071bf70
j-lazo <j.org.ls@ciencias.unam.mx>
2021-05-14 10:37:38 +0200

    reconfigure functions layaout

--- scripts/em_tracking/sample_test.py
+++ scripts/em_tracking/sample_test.py
@@ -1,18 +1,27 @@
 from sksurgerynditracker.nditracker import NDITracker
 import os
-print(os.getcwd())
 
-SETTINGS = {
-    "tracker type": "aurora",
-    "romfiles": [''.join([os.getcwd(), '/scripts/em_tracking/080082.rom'])]
-        }
-TRACKER = NDITracker(SETTINGS)
 
-TRACKER.start_tracking()
-port_handles, timestamps, framenumbers, tracking, quality = TRACKER.get_frame()
-print(port_handles, timestamps, framenumbers, tracking, quality)
-for t in tracking:
-  print(t)
+def test(tracker):
 
-TRACKER.stop_tracking()
-TRACKER.close()
+    port_handles, timestamps, framenumbers, tracking, quality = tracker.get_frame()
+    for t in tracking:
+        print(t)
+
+
+def main():
+
+    SETTINGS = {
+        "tracker type": "aurora",
+        "romfiles": [''.join([os.getcwd(), '/scripts/em_tracking/080082.rom'])]
+    }
+
+    TRACKER = NDITracker(SETTINGS)
+    TRACKER.start_tracking()
+    test(TRACKER)
+    TRACKER.stop_tracking()
+    TRACKER.close()
+
+
+if __name__ == '__main__':
+    main()
