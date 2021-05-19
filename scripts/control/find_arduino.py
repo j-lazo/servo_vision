71faae84ad76568bf5e803df65f1c37381374fd7
j-lazo <j.org.ls@ciencias.unam.mx>
2021-05-19 19:42:21 +0200

    script to find arduino

---
+++ scripts/control/find_arduino.py
@@ -0,0 +1,41 @@
+import warnings
+import serial
+import serial.tools.list_ports
+import platform
+import glob
+
+def find_arduino():
+
+    os_name = platform.system()
+    print('OS:', os_name)
+
+    if os_name == 'Windows':
+
+        ports = [p.device
+                        for p in serial.tools.list_ports.comports()
+                        if 'Arduino' in p.description]
+
+        if not ports:
+            raise IOError("No Arduino found")
+
+        if len(ports) > 1:
+            warnings.warn("Multiple Arduinos found - using the first")
+
+        ser = serial.Serial(ports[0])
+
+    elif os_name == 'Linux':
+        glist = glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*')
+        if not glist:
+            raise IOError("No Arduino found")
+
+        if len(glist) > 1:
+            warnings.warn("Multiple Arduinos found - using the first")
+
+        ser = serial.Serial(glist[0])
+
+    else:
+        raise IOError("OS detected not compatible with script")
+        ser = 'naN'
+
+    return ser
+
