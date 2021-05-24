8439b115c35ff59c036ee037ab8b3d3719d39b17
j-lazo <j.org.ls@ciencias.unam.mx>
2021-05-24 15:44:37 +0200

    visualize center of ther circles and delta

--- scripts/computer_vision/calibration/detect_circle.py
+++ scripts/computer_vision/calibration/detect_circle.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 
 
-def detect_circle(frame):
+def detect_circle(frame, delta):
+    w, h, d = np.shape(frame)
     x, y = 0, 0
+
     gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     output = frame.copy()
     circles = cv2.HoughCircles(gray_img, cv2.HOUGH_GRADIENT,
@@ -18,7 +20,11 @@ def detect_circle(frame):
             # draw the circle in the output image, then draw a rectangle
             # corresponding to the center of the circle
             cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+            cv2.circle(output, (int(h/2), int(w/2)), delta, (0, 0, 255), 4)
+            cv2.rectangle(output, (int(w/2) - 5, int(h/2) - 5), (int(w/2) + 5, int(h/2) + 5), (0, 0, 255), -1)
             cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+            cv2.line(output, (0, int(w/2)), (h, int(w/2)), (0, 255, 255), 2)
+            cv2.line(output, (int(h/2), 0), (int(h/2), w), (0, 255, 255), 2)
         # show the output image
 
     return output, [x, y]
