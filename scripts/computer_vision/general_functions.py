corrected average detection

scripts/computer_vision/general_functions.py
@@ -38,8 +38,8 @@ def detect_corners_chessboard(frame):
 
     else:
         output_image = gray
-        point_x = 'Nan'
-        point_y = 'Nan'
+        point_x = np.nan
+        point_y = np.nan
 
     return output_image, point_x, point_y
 

scripts/general_functions/data_managament.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 
 def calculate_average_points(list_of_points):
-    clean_list = [point for point in list_of_points if not(not(np.isnan(point)))]
+    clean_list = [point for point in list_of_points if point != np.nan]
     average_point = np.mean(clean_list)
     return average_point
 