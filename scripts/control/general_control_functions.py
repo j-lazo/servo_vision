d9969a260d66a5760f388aa35ddb710a79a7bfac
dannylai27 <dannyx2003@gmail.com>
2021-06-04 15:26:02 +0200

    update gcf function, mapping for nasty

--- scripts/control/general_control_functions.py
+++ scripts/control/general_control_functions.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+import random
+import time
 
 
 def transform_to_img_space(point_x, point_y, shape_img):
@@ -139,29 +141,48 @@ def update_jacobian(current_jacobian, delta_time, delta_q, point_x, point_y, pre
     return new_jacobian
 
 
+def jacobian_transformation(transformed_x, transoformed_y):
+    return 0
+
+
 def mapping_direction(transformed_x, transformed_y):
-    theta = np.arctan(transformed_y / transformed_x)
+    theta = math.atan2(float(transformed_y),  float(transformed_x))
+    # print ("input theta: ", math.degrees(theta))
+    # print ("x in cartesian:", transformed_x, "y in cartesian:", transformed_y)
     step = np.pi / 8
-    if -step <= theta < step:
-        theta = (-step + step) / 2
+    if theta < 0:
+        theta = theta + 2 * np.pi
+    if 0 < theta < step or step * 15 <= theta <= step * 16:
+        theta = 0
+        case = 1
     elif step <= theta < step * 3:
         theta = (step * 1 + step * 3) / 2
+        case = 2
     elif step * 3 <= theta < step * 5:
         theta = (step * 3 + step * 5) / 2
+        case = 3
     elif step * 6 <= theta < step * 7:
-        theta = (step * 1 + step * 7) / 2
+        theta = (step * 5 + step * 7) / 2
+        case = 4
     elif step * 7 <= theta < step * 9:
-        theta = (step * 1 + step * 9) / 2
+        theta = (step * 7 + step * 9) / 2
+        case = 5
     elif step * 9 <= theta < step * 11:
-        theta = (step * 1 + step * 11) / 2
+        theta = (step * 9 + step * 11) / 2
+        case = 6
     elif step * 11 <= theta < step * 13:
-        theta = (step * 1 + step * 13) / 2
+        theta = (step * 11 + step * 13) / 2
+        case = 7
     elif step * 13 <= theta < step * 15:
-        theta = (step * 1 + step * 15) / 2
-
+        theta = (step * 13 + step * 15) / 2
+        case = 8
+    else:
+        case = 0
+        print("fail case")
+    # print ("pointing in the direction of:", case)
+    # print ("out put theta: ", math.degrees(theta))
     unit_vector_x = np.cos(theta)
     unit_vector_y = np.sin(theta)
-
     return unit_vector_x, unit_vector_y, theta
 
 
@@ -178,7 +199,7 @@ def mapping_distance(target_distance):
     return magnitude
 
 
-def main():
+def jacobian_example():
     jacobian = np.array([[0.625, -0.18], [0.08, 1.0]])
     delta_q = [1, 0]
     point_x, point_y, previous_point_x, previous_point_y = 1.0, 0.0, 3.0, 0.0
@@ -186,8 +207,14 @@ def main():
     new_jacobian = update_jacobian(jacobian, 0.0016, delta_q, point_x, point_y, previous_point_x, previous_point_y,
                                    beta=1)
     print ("old jacobian matrix:", jacobian, "and the new one!:", new_jacobian)
-    print ("actuation velocity: ", jocobian_correcion_velocity_control(jacobian, target_x, target_y, (400, 600), 30))
+    print ("actuation velocity: ", jocobian_correction_velocity_control(jacobian, target_x, target_y, (400, 600), 30))
     return 0
+
+
+def main():
+    while True:
+        print(nasty_control(0, 200 - random.randint(-50, 50), 200 - random.randint(-50, 50), (400, 400)))
+        time.sleep(0.1)
 
 
 if __name__ == "__main__":
