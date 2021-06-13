2c88af17a7b421085eb90990b374146cd36fbd75
Jorge Lazo <jorgefrancisco.lazo@polimi.it>
2021-06-14 00:40:05 +0200

    draw lumen

--- scripts/general_functions/data_analysis.py
+++ scripts/general_functions/data_analysis.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import glob
 
+
 def build_ground_truth_paths(directory_files, plot=True):
 
     list_files = os.listdir(directory_files)
@@ -17,14 +18,33 @@ def build_ground_truth_paths(directory_files, plot=True):
         list_all_y.append(data['sensor y'].tolist())
         list_all_z.append(data['sensor z'].tolist())
 
-        print(data['sensor z'].tolist())
+        #print(data['sensor z'].tolist())
 
     min_y, max_y = np.amin(np.amin(list_all_z)), np.amax(np.amax(list_all_z))
     print(min_y, max_y)
+    min_x, max_x = np.amin(np.amin(list_all_y)), np.amax(np.amax(list_all_y))
+    print(min_x, max_x)
+    line_x = list(range(int(min_x), int(max_x)))
+    line_y = np.ones((len(line_x)))*(min_y + (max_y-min_y)*0.5)
+    upper_lim_1 = [line + 5 for line in line_y]
+    upper_lim_2 = [line + 10 for line in line_y]
+    down_lim_1 = [line - 5 for line in line_y]
+    down_lim_2 = [line - 10 for line in line_y]
+    vertical_left_x = [line_x[0], line_x[0]]
+    vertical_left_y = [down_lim_1[0], upper_lim_1[0]]
+
+    vertical_right_x = [line_x[-1], line_x[-1]]
+    vertical_right_y = [down_lim_1[0], upper_lim_1[0]]
+
     if plot is True:
         plt.figure()
         for j in range(len(list_all_z)):
             plt.plot(list_all_y[j], list_all_z[j], 'o-')
+            plt.plot(line_x, line_y, 'r--')
+            plt.plot(line_x, down_lim_1, color='darkorange')
+            plt.plot(line_x, upper_lim_1, color='darkorange')
+            plt.plot(vertical_left_x, vertical_left_y, '--')
+            plt.plot(vertical_right_x, vertical_right_y, '--')
             plt.ylim([min_y-30, max_y+30])
         """fig1, axs1 = plt.subplots(len(list_files), 1, figsize=(17, 8))
         for j, model in enumerate(list_files):
@@ -41,7 +61,6 @@ def build_ground_truth_paths(directory_files, plot=True):
 
 
 def analyze_results(directory_files, plot=True):
-
     results_csv = []
     list_sub_folders = sorted(os.listdir(directory_files))
     for sub_folder in list_sub_folders:
@@ -89,6 +108,8 @@ def analyze_results(directory_files, plot=True):
 
 
 if __name__ == '__main__':
-    directory = '/home/nearlab/Jorge/current_work/robot_vision/results/n_control/right_curve/'
-    analyze_results(directory)
+    #directory = os.getcwd() + '/results/n_control/right_curve/'
+    #analyze_results(directory)
+    directory = os.getcwd() + '/data/calibration/gt_trajectories/straight_line/'
+    build_ground_truth_paths(directory)
     plt.show()
