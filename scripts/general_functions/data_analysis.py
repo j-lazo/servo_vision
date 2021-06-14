c8a4342b6f4ea3508b978de4f76c782dabb4471b
j-lazo <j.org.ls@ciencias.unam.mx>
2021-06-14 15:24:20 +0200

    added comapre gt with experiments data

--- scripts/general_functions/data_analysis.py
+++ scripts/general_functions/data_analysis.py
@@ -3,6 +3,34 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 import glob
+import time
+
+
+def discretise_series_horizontal(data_list):
+    min_value = np.amin([np.amin(sub_list) for sub_list in data_list])
+    max_value = np.amax([np.amax(sub_list) for sub_list in data_list])
+    num_points = (int(max_value)+1 - (int(min_value)-1)) * 2
+    discretised_series = np.linspace(int(min_value)-1, int(max_value)+1, num_points, endpoint=False)
+    return list(discretised_series)
+
+
+def discretise_series_vertical(discreet_x, list_points_x, list_points_y):
+    print(np.shape(discreet_x))
+    discreet_y = [[] for _ in range(len(discreet_x))]
+    for index_list, originals_horizontal in enumerate(list_points_x):
+        for index_point, point in enumerate(originals_horizontal):
+            for point_l in discreet_x:
+                if point_l <= point < point_l + 0.5:
+                    index_discreet = (discreet_x.index(point_l))
+                    discreet_y[index_discreet].append(list_points_y[index_list][index_point])
+
+    for j, sub_list in enumerate(discreet_y):
+        if sub_list == []:
+            discreet_y[j] = discreet_y[j-1]
+    mean_y = [np.mean(sub_list) for sub_list in discreet_y]
+    stdv_y = [np.std(sub_list) for sub_list in discreet_y]
+
+    return discreet_y, mean_y, stdv_y
 
 
 def build_ground_truth_paths(directory_files, plot=True):
@@ -18,39 +46,42 @@ def build_ground_truth_paths(directory_files, plot=True):
         list_all_y.append(data['sensor y'].tolist())
         list_all_z.append(data['sensor z'].tolist())
 
-        #print(data['sensor z'].tolist())
+    min_y = np.amin([np.amin(sub_list) for sub_list in list_all_z])
+    max_y = np.amax([np.amax(sub_list) for sub_list in list_all_z])
+    min_x = np.amin([np.amin(sub_list) for sub_list in list_all_y])
+    max_x = np.amax([np.amax(sub_list) for sub_list in list_all_y])
 
-    min_y, max_y = np.amin(np.amin(list_all_z)), np.amax(np.amax(list_all_z))
-    print(min_y, max_y)
-    min_x, max_x = np.amin(np.amin(list_all_y)), np.amax(np.amax(list_all_y))
-    print(min_x, max_x)
-    line_x = list(range(int(min_x), int(max_x)))
+    inner_radius = 5
+    outer_radius = 10
+
+    line_x = list(range(int(min_x)-2, int(max_x)+2))
     line_y = np.ones((len(line_x)))*(min_y + (max_y-min_y)*0.5)
-    upper_lim_1 = [line + 5 for line in line_y]
-    upper_lim_2 = [line + 10 for line in line_y]
-    down_lim_1 = [line - 5 for line in line_y]
-    down_lim_2 = [line - 10 for line in line_y]
-    vertical_left_x = [line_x[0], line_x[0]]
-    vertical_left_y = [down_lim_1[0], upper_lim_1[0]]
+    upper_lim_1 = [line + inner_radius for line in line_y]
+    upper_lim_2 = [line + outer_radius for line in line_y]
+    down_lim_1 = [line - inner_radius for line in line_y]
+    down_lim_2 = [line - outer_radius for line in line_y]
 
-    vertical_right_x = [line_x[-1], line_x[-1]]
-    vertical_right_y = [down_lim_1[0], upper_lim_1[0]]
-    paint_area = (list(zip(line_x, upper_lim_1)))
-    paint_area = paint_area + (list(zip(line_x, down_lim_1)))
-    print(paint_area)
+    discrete_series_x = discretise_series_horizontal(list_all_y)
+    discreet_y, mean_y, stdv_y = discretise_series_vertical(discrete_series_x, list_all_y, list_all_z)
+    up_error = [mean_value + stdv_y[j] for j, mean_value in enumerate(mean_y)]
+    down_error = [mean_value - stdv_y[j] for j, mean_value in enumerate(mean_y)]
+
     if plot is True:
         plt.figure()
-        for j in range(len(list_all_z)):
-            plt.plot(list_all_y[j], list_all_z[j], 'o-')
-            plt.plot(line_x, line_y, 'r--')
-            plt.plot(line_x, down_lim_1, color='darkorange')
-            plt.plot(line_x, upper_lim_1, color='darkorange')
-            plt.plot(line_x, upper_lim_2, color='darkorange')
-            plt.plot(line_x, down_lim_2, color='darkorange')
-            plt.plot(vertical_left_x, vertical_left_y, '--')
-            plt.plot(vertical_right_x, vertical_right_y, '--')
-            plt.fill([(60, -160), (120, -130), (80, -150), (80, -140)])
-            plt.ylim([min_y-30, max_y+30])
+        #for j in range(len(list_all_z)):
+        #    plt.plot(list_all_y[j], list_all_z[j], 'o')
+        plt.plot(discrete_series_x, mean_y, '--')
+        plt.plot(line_x, down_lim_1, color='darkorange')
+        plt.plot(line_x, upper_lim_1, color='darkorange')
+        plt.plot(line_x, upper_lim_2, color='darkorange')
+        plt.plot(line_x, down_lim_2, color='darkorange')
+        plt.fill_between(line_x, upper_lim_1, down_lim_1, alpha=0.4, color='bisque')
+        plt.fill_between(line_x, upper_lim_2, upper_lim_1, alpha=0.8, color='darkorange')
+        plt.fill_between(line_x, down_lim_1, down_lim_2, alpha=0.8, color='darkorange')
+        plt.fill_between(discrete_series_x, up_error, down_error, alpha=0.3, color='blue')
+
+        plt.ylim([min_y-30, max_y+30])
+            #plt.axis('off')
         """fig1, axs1 = plt.subplots(len(list_files), 1, figsize=(17, 8))
         for j, model in enumerate(list_files):
             for i in range(len(list_all_z)):
@@ -63,6 +94,96 @@ def build_ground_truth_paths(directory_files, plot=True):
                 #if j != 0:
                 #    axs1[i, j].set_yticks([])
                 #axs1[i, j].set_xticks([])"""
+
+
+def compare_gt_and_results(directory_files, results_directory, plot=True):
+
+    list_files = os.listdir(directory_files)
+    list_files = sorted([file for file in list_files if file.endswith('.csv')])
+    list_all_x = []
+    list_all_y = []
+    list_all_z = []
+    for file in list_files:
+        data = pd.read_csv(directory_files + file, skipinitialspace=True)
+        list_all_x.append(data['sensor x'].tolist())
+        list_all_y.append(data['sensor y'].tolist())
+        list_all_z.append(data['sensor z'].tolist())
+
+    min_y = np.amin([np.amin(sub_list) for sub_list in list_all_z])
+    max_y = np.amax([np.amax(sub_list) for sub_list in list_all_z])
+    min_x = np.amin([np.amin(sub_list) for sub_list in list_all_y])
+    max_x = np.amax([np.amax(sub_list) for sub_list in list_all_y])
+
+    inner_radius = 8
+    outer_radius = 14
+
+    line_x = list(range(int(min_x)-2, int(max_x)+2))
+    line_y = np.ones((len(line_x)))*(min_y + (max_y-min_y)*0.5)
+    upper_lim_1 = [line + inner_radius for line in line_y]
+    upper_lim_2 = [line + outer_radius for line in line_y]
+    down_lim_1 = [line - inner_radius for line in line_y]
+    down_lim_2 = [line - outer_radius for line in line_y]
+
+    discrete_series_x = discretise_series_horizontal(list_all_y)
+    discreet_y, mean_y, stdv_y = discretise_series_vertical(discrete_series_x, list_all_y, list_all_z)
+    up_error = [mean_value + stdv_y[j]*2 for j, mean_value in enumerate(mean_y)]
+    down_error = [mean_value - stdv_y[j]*2 for j, mean_value in enumerate(mean_y)]
+
+    x_curve = np.linspace(line_x[-1], line_x[-1] + inner_radius, 180)
+    x_curve_2 = np.linspace(line_x[-1], line_x[-1] + outer_radius, 180)
+
+    y_curve = [np.sqrt(inner_radius**2-(x-line_x[-1])**2)+line_y[-1] for x in x_curve]
+    y_curve_2 = [-np.sqrt(inner_radius ** 2 - (x - line_x[-1]) ** 2) + line_y[-1] for x in x_curve]
+
+    y_curve_3 = [np.sqrt(outer_radius**2-(x-line_x[-1])**2)+line_y[-1] for x in x_curve_2]
+    y_curve_4 = [-np.sqrt(outer_radius ** 2 - (x - line_x[-1]) ** 2) + line_y[-1] for x in x_curve_2]
+
+    results_csv = []
+    list_sub_folders = sorted(os.listdir(results_directory))
+    for sub_folder in list_sub_folders:
+        results_csv.append(glob.glob(os.path.join(results_directory, sub_folder) + '/*.csv')[0])
+
+    list_files = os.listdir(results_directory)
+    list_files = [file for file in list_files if file.endswith('.csv')]
+    list_all_x = []
+    list_all_y = []
+    list_all_z = []
+    for csv_file in results_csv:
+        data = pd.read_csv(csv_file, skipinitialspace=True)
+        list_all_x.append(data['sensor x'].tolist())
+        list_all_y.append(data['sensor y'].tolist())
+        list_all_z.append(data['sensor z'].tolist())
+
+    if plot is True:
+        plt.figure()
+        plt.plot(discrete_series_x, mean_y, '--')
+        plt.plot(line_x, line_y, 'r--')
+        plt.plot(line_x, down_lim_1, color='darkorange')
+        plt.plot(line_x, upper_lim_1, color='darkorange')
+        plt.plot(line_x, upper_lim_2, color='darkorange')
+        plt.plot(line_x, down_lim_2, color='darkorange')
+
+        plt.plot(x_curve, y_curve, color='darkorange')
+        plt.plot(x_curve, y_curve_2, color='darkorange')
+        plt.plot(x_curve_2, y_curve_3, color='darkorange')
+        plt.plot(x_curve_2, y_curve_4, color='darkorange')
+        #plt.fill_between(x_curve_2, y_curve, y_curve_3, alpha=0.8, color='darkorange')
+        plt.fill_between(x_curve_2, y_curve_3, y_curve_4, alpha=0.2, color='orange')
+        plt.fill_between(x_curve, y_curve, y_curve_2, alpha=0.9, color='bisque')
+
+
+
+        plt.fill_between(line_x, upper_lim_1, down_lim_1, alpha=0.4, color='bisque')
+        plt.fill_between(line_x, upper_lim_2, upper_lim_1, alpha=0.8, color='darkorange')
+        plt.fill_between(line_x, down_lim_1, down_lim_2, alpha=0.8, color='darkorange')
+        plt.fill_between(discrete_series_x, up_error, down_error, alpha=0.3, color='blue')
+
+        for j in range(len(list_all_z)):
+            plt.plot(list_all_y[j], list_all_z[j], 'o-')
+
+        plt.ylim([min_y - 30, max_y + 30])
+        plt.xlabel('position x')
+        plt.ylabel('position y')
 
 
 def analyze_results(directory_files, plot=True):
@@ -85,10 +206,7 @@ def analyze_results(directory_files, plot=True):
         list_all_y.append(data['sensor y'].tolist())
         list_all_z.append(data['sensor z'].tolist())
 
-        print(data['sensor z'].tolist())
-
     min_y, max_y = np.amin(np.amin(list_all_z)), np.amax(np.amax(list_all_z))
-    print(min_y, max_y)
     if plot is True:
         plt.figure()
         for j in range(len(list_all_z)):
@@ -98,6 +216,7 @@ def analyze_results(directory_files, plot=True):
             #plt.ylim([-250, 150])
             plt.subplot(2, 1, 2)
             plt.plot(list_all_x[j], 'o-')
+
         """fig1, axs1 = plt.subplots(len(list_files), 1, figsize=(17, 8))
         for j, model in enumerate(list_files):
             for i in range(len(list_all_z)):
@@ -113,8 +232,8 @@ def analyze_results(directory_files, plot=True):
 
 
 if __name__ == '__main__':
-    #directory = os.getcwd() + '/results/n_control/right_curve/'
+    directory_2 = os.getcwd() + '/results/n_control/straight_line/'
     #analyze_results(directory)
-    directory = os.getcwd() + '/data/calibration/gt_trajectories/straight_line/'
-    build_ground_truth_paths(directory)
+    directory_1 = os.getcwd() + '/data/calibration/gt_trajectories/straight_line/'
+    compare_gt_and_results(directory_1, directory_2)
     plt.show()
