4a65d2f131ef19a7b103751263ec205740c7cf45
j-lazo <j.org.ls@ciencias.unam.mx>
2021-06-15 16:46:15 +0200

    plot error at each axxis added

--- scripts/general_functions/data_analysis.py
+++ scripts/general_functions/data_analysis.py
@@ -33,6 +33,26 @@ def discretise_series_vertical(discreet_x, list_points_x, list_points_y):
     return discreet_y, mean_y, stdv_y
 
 
+def discretise_single_series_vertical(teoretical_series, list_points_series):
+    print(teoretical_series)
+    print(list_points_series)
+    discreet_series = [[] for _ in range(len(teoretical_series))]
+    error_sries = [[] for _ in range(len(teoretical_series))]
+
+    for index_point, point in enumerate(list_points_series):
+        for point_l in teoretical_series:
+            if point_l <= point < point_l + 0.5:
+                index_discreet = (teoretical_series.index(point_l))
+                discreet_series[index_discreet].append(list_points_series[index_point])
+
+    for j, sub_list in enumerate(discreet_series):
+        if sub_list == []:
+            print('True')
+            discreet_series[j] = discreet_series[j-1]
+
+    return discreet_series, error_sries
+
+
 def build_ground_truth_paths(directory_files, plot=True):
 
     list_files = os.listdir(directory_files)
@@ -96,6 +116,112 @@ def build_ground_truth_paths(directory_files, plot=True):
                 #axs1[i, j].set_xticks([])"""
 
 
+def plot_3D_trajectory(directory_results, directory_file):
+
+    list_files = os.listdir(directory_results)
+    list_files = sorted([file for file in list_files if file.endswith('.csv')])
+    list_all_x = []
+    list_all_y = []
+    list_all_z = []
+    for file in list_files:
+        data = pd.read_csv(directory_results + file, skipinitialspace=True)
+        list_all_x.append(data['sensor x'].tolist())
+        list_all_y.append(data['sensor y'].tolist())
+        list_all_z.append(data['sensor z'].tolist())
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    min_y = np.amin([np.amin(sub_list) for sub_list in list_all_z])
+    max_y = np.amax([np.amax(sub_list) for sub_list in list_all_z])
+    min_x = np.amin([np.amin(sub_list) for sub_list in list_all_y])
+    max_x = np.amax([np.amax(sub_list) for sub_list in list_all_y])
+
+    inner_radius = 8
+    outer_radius = 12
+
+    discrete_series_x = discretise_series_horizontal(list_all_y)
+    discreet_y, mean_y, stdv_y = discretise_series_vertical(discrete_series_x, list_all_y, list_all_z)
+    discreet_x, mean_x, stdv_x = discretise_series_vertical(discrete_series_x, list_all_y, list_all_x)
+
+    # Cylinder
+    x_cilinder = np.linspace(68, 84, 500)
+    z = np.linspace(min_x - 7, max_x + 7, 500)
+    Xc, Zc = np.meshgrid(x_cilinder, z)
+    Yc1 = np.sqrt(inner_radius ** 2 - (76 - Xc) ** 2) - 158
+    Yc2 = -np.sqrt(inner_radius ** 2 - (76 - Xc) ** 2) - 158
+
+    out_x_cilinder = np.linspace(64, 88, 500)
+    z = np.linspace(min_x - 7, max_x + 7, 500)
+    Xc2, Zc2 = np.meshgrid(out_x_cilinder, z)
+    Yc3 = np.sqrt(outer_radius ** 2 - (76 - Xc2) ** 2) - 158
+    Yc4 = -np.sqrt(outer_radius ** 2 - (76 - Xc2) ** 2) - 158
+
+    x_line = np.ones(500)*76
+    z_line = np.ones(500)*-158
+    y_line = np.linspace(-15, 140, 500)
+
+    # Draw parameters
+    rstride = 10
+    cstride = 20
+    # plot the cylinders
+    ax.plot_wireframe(Xc, Zc, Yc1, alpha=0.2, rstride=rstride, cstride=cstride, color='orange')
+    ax.plot_wireframe(Xc, Zc, Yc2, alpha=0.2, rstride=rstride, cstride=cstride, color='orange')
+    ax.plot_wireframe(Xc2, Zc2, Yc3, alpha=0.1, rstride=rstride, cstride=cstride, color='orange' )
+    ax.plot_wireframe(Xc2, Zc2, Yc4, alpha=0.1, rstride=rstride, cstride=cstride, color='orange')
+
+    ax.plot(x_line, y_line, z_line, 'r-o')
+    for j in range(len(list_all_x)):
+        ax.plot(list_all_x[j], list_all_y[j], list_all_z[j], '-o', label='trajectory ' +str(j))
+    ax.legend()
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    ax.set_xlim(55, 95)
+    plt.show()
+
+
+def calcualte_error_list(ground_truth, measured):
+    ground_truth = list(ground_truth)
+    discreet_list = [[] for _ in range(len(ground_truth))]
+
+    for index_point, point in enumerate(measured):
+        for point_l in ground_truth:
+            if point_l <= point < point_l + 0.5:
+                index_discreet = (ground_truth.index(point_l))
+                discreet_list[index_discreet].append(measured[index_point])
+
+    for j, sub_list in enumerate(discreet_list):
+        if sub_list == []:
+            discreet_list[j] = discreet_list[j-1]
+
+    mean_y = [np.mean(sub_list) for sub_list in discreet_list]
+    abs_error_list = [(np.abs(value-ground_truth[j])) for j, value in enumerate(mean_y)]
+
+    return abs_error_list
+
+
+def calcualte_error_dependant_list(ground_truth, measured, auxiliar_list):
+
+    ground_truth = list(ground_truth)
+    discreet_list = [[] for _ in range(len(ground_truth))]
+
+    for index_point, point in enumerate(measured):
+        for point_l in ground_truth:
+            #print(point_l, point)
+            if point_l <= point < point_l + 5:
+                index_discreet = (ground_truth.index(point_l))
+                discreet_list[index_discreet].append(measured[index_point])
+
+    for j, sub_list in enumerate(discreet_list):
+        if sub_list == []:
+            discreet_list[j] = discreet_list[j-1]
+
+    mean_y = [np.mean(sub_list) for sub_list in discreet_list]
+    abs_error_list = [(np.abs(value-ground_truth[j])) for j, value in enumerate(mean_y)]
+    return abs_error_list
+
+
 def compare_gt_and_results(directory_files, results_directory, plot=True):
 
     list_files = os.listdir(directory_files)
@@ -114,29 +240,40 @@ def compare_gt_and_results(directory_files, results_directory, plot=True):
     min_x = np.amin([np.amin(sub_list) for sub_list in list_all_y])
     max_x = np.amax([np.amax(sub_list) for sub_list in list_all_y])
 
+    min_z = np.amin([np.amin(sub_list) for sub_list in list_all_x])
+    max_z = np.amax([np.amax(sub_list) for sub_list in list_all_x])
+
     inner_radius = 8
     outer_radius = 14
 
-    line_x = list(range(int(min_x)-2, int(max_x)+2))
-    line_y = np.ones((len(line_x)))*(min_y + (max_y-min_y)*0.5)
-    upper_lim_1 = [line + inner_radius for line in line_y]
-    upper_lim_2 = [line + outer_radius for line in line_y]
-    down_lim_1 = [line - inner_radius for line in line_y]
-    down_lim_2 = [line - outer_radius for line in line_y]
-
     discrete_series_x = discretise_series_horizontal(list_all_y)
     discreet_y, mean_y, stdv_y = discretise_series_vertical(discrete_series_x, list_all_y, list_all_z)
+    discreet_z, mean_z, stdv_z = discretise_series_vertical(discrete_series_x, list_all_y, list_all_x)
+
+
     up_error = [mean_value + stdv_y[j]*2 for j, mean_value in enumerate(mean_y)]
     down_error = [mean_value - stdv_y[j]*2 for j, mean_value in enumerate(mean_y)]
 
-    x_curve = np.linspace(line_x[-1], line_x[-1] + inner_radius, 180)
-    x_curve_2 = np.linspace(line_x[-1], line_x[-1] + outer_radius, 180)
+    line_x = np.linspace(int(min_x)-2, int(max_x)+2, len(mean_y))
+    line_y = np.ones((len(line_x)))*(min_y + (max_y-min_y)*0.5)
+    line_z = np.ones((len(line_x)))*(min_z + (max_z-min_z)*0.5)
 
-    y_curve = [np.sqrt(inner_radius**2-(x-line_x[-1])**2)+line_y[-1] for x in x_curve]
-    y_curve_2 = [-np.sqrt(inner_radius ** 2 - (x - line_x[-1]) ** 2) + line_y[-1] for x in x_curve]
+    print(np.shape(line_x))
+    print(np.shape(line_y))
 
-    y_curve_3 = [np.sqrt(outer_radius**2-(x-line_x[-1])**2)+line_y[-1] for x in x_curve_2]
-    y_curve_4 = [-np.sqrt(outer_radius ** 2 - (x - line_x[-1]) ** 2) + line_y[-1] for x in x_curve_2]
+    upper_lim_1 = [line + inner_radius for line in mean_y]
+    upper_lim_2 = [line + outer_radius for line in mean_y]
+    down_lim_1 = [line - inner_radius for line in mean_y]
+    down_lim_2 = [line - outer_radius for line in mean_y]
+
+    x_curve = np.linspace(discrete_series_x[-1], discrete_series_x[-1] + inner_radius, 180)
+    x_curve_2 = np.linspace(discrete_series_x[-1], discrete_series_x[-1] + outer_radius, 180)
+
+    y_curve = [np.sqrt(inner_radius**2-(x-discrete_series_x[-1])**2)+mean_y[-1] for x in x_curve]
+    y_curve_2 = [-np.sqrt(inner_radius ** 2 - (x - discrete_series_x[-1]) ** 2) + mean_y[-1] for x in x_curve]
+
+    y_curve_3 = [np.sqrt(outer_radius**2-(x-discrete_series_x[-1])**2)+mean_y[-1] for x in x_curve_2]
+    y_curve_4 = [-np.sqrt(outer_radius ** 2 - (x - line_x[-1]) ** 2) + mean_y[-1] for x in x_curve_2]
 
     results_csv = []
     list_sub_folders = sorted(os.listdir(results_directory))
@@ -156,34 +293,67 @@ def compare_gt_and_results(directory_files, results_directory, plot=True):
 
     if plot is True:
         plt.figure()
-        plt.plot(discrete_series_x, mean_y, '--')
-        plt.plot(line_x, line_y, 'r--')
-        plt.plot(line_x, down_lim_1, color='darkorange')
-        plt.plot(line_x, upper_lim_1, color='darkorange')
-        plt.plot(line_x, upper_lim_2, color='darkorange')
-        plt.plot(line_x, down_lim_2, color='darkorange')
+        plt.plot(discrete_series_x[6:], mean_y[6:], '--')
+        plt.fill_between(discrete_series_x, up_error, down_error, alpha=0.3, color='blue')
+
+        #plt.plot(line_x, line_y, 'r--')
+        plt.plot(discrete_series_x, down_lim_1, color='darkorange',  linewidth=3)
+        plt.plot(discrete_series_x, upper_lim_1, color='darkorange', linewidth=3)
+        plt.plot(discrete_series_x, upper_lim_2, color='darkorange', linewidth=3)
+        plt.plot(discrete_series_x, down_lim_2, color='darkorange', linewidth=3)
 
         plt.plot(x_curve, y_curve, color='darkorange')
         plt.plot(x_curve, y_curve_2, color='darkorange')
         plt.plot(x_curve_2, y_curve_3, color='darkorange')
         plt.plot(x_curve_2, y_curve_4, color='darkorange')
-        #plt.fill_between(x_curve_2, y_curve, y_curve_3, alpha=0.8, color='darkorange')
+        plt.fill_between(x_curve_2, y_curve, y_curve_3, alpha=0.8, color='darkorange')
         plt.fill_between(x_curve_2, y_curve_3, y_curve_4, alpha=0.2, color='orange')
         plt.fill_between(x_curve, y_curve, y_curve_2, alpha=0.9, color='bisque')
 
-
-
-        plt.fill_between(line_x, upper_lim_1, down_lim_1, alpha=0.4, color='bisque')
-        plt.fill_between(line_x, upper_lim_2, upper_lim_1, alpha=0.8, color='darkorange')
-        plt.fill_between(line_x, down_lim_1, down_lim_2, alpha=0.8, color='darkorange')
-        plt.fill_between(discrete_series_x, up_error, down_error, alpha=0.3, color='blue')
+        plt.fill_between(discrete_series_x, upper_lim_1, down_lim_1, alpha=0.4, color='bisque')
+        plt.fill_between(discrete_series_x, upper_lim_2, upper_lim_1, alpha=0.8, color='darkorange')
+        plt.fill_between(discrete_series_x, down_lim_1, down_lim_2, alpha=0.8, color='darkorange')
 
         for j in range(len(list_all_z)):
             plt.plot(list_all_y[j], list_all_z[j], 'o-')
 
         plt.ylim([min_y - 30, max_y + 30])
-        plt.xlabel('position x')
-        plt.ylabel('position y')
+        plt.xlabel('position y')
+        plt.ylabel('position x')
+
+        plt.figure()
+        #plt.title('Error Y axis')
+        for j in range(len(list_all_z)):
+            line_temp = list_all_y[j][::-1]
+            plot_list = calcualte_error_list(discrete_series_x, line_temp)
+            plt.plot(plot_list, 'o-', label='trajectory '+str(j))
+            plt.ylabel('Error Y (mm)')
+            plt.legend(loc='best')
+            plt.xlabel('t')
+
+        plt.figure()
+        # plt.title('Error Y axis')
+        for j in range(len(list_all_z)):
+            line_temp = list_all_x[j]#[::-1]
+            #plot_list = [np.abs(x - line_z[0]) for x in line_temp]
+            plot_list = calcualte_error_dependant_list(mean_z, line_temp, discrete_series_x)
+            plt.plot(discrete_series_x, plot_list,  'o-', label='trajectory '+str(j))
+            plt.ylabel('Absolute error X (mm)')
+            plt.legend(loc='best')
+            plt.xlim(max(discrete_series_x), min(discrete_series_x))
+            plt.xlabel('y (mm)')
+
+        plt.figure()
+        # plt.title('Error Y axis')
+        for j in range(len(list_all_z)):
+            line_temp = list_all_z[j]#[::-1]
+            #plot_list = [np.abs(x - line_y[0]) for x in line_temp]
+            plot_list = calcualte_error_dependant_list(mean_y, line_temp, discrete_series_x)
+            plt.plot(discrete_series_x, plot_list, 'o-', label='trajectory '+str(j))
+            plt.ylabel('Absolute error Z (mm)')
+            plt.legend(loc='best')
+            plt.gca().invert_xaxis()
+            plt.xlabel('y (mm)')
 
 
 def analyze_results(directory_files, plot=True):
@@ -210,7 +380,7 @@ def analyze_results(directory_files, plot=True):
     if plot is True:
         plt.figure()
         for j in range(len(list_all_z)):
-            plt.subplot(2,1,1)
+            plt.subplot(2, 1, 1)
             plt.plot(list_all_z[j], 'o-')
             #plt.plot(list_all_y[j], list_all_z[j], 'o-')
             #plt.ylim([-250, 150])
@@ -232,8 +402,8 @@ def analyze_results(directory_files, plot=True):
 
 
 if __name__ == '__main__':
-    directory_2 = os.getcwd() + '/results/n_control/straight_line/'
+    directory_2 = os.getcwd() + '/results/n_control/left_curve/'
     #analyze_results(directory)
-    directory_1 = os.getcwd() + '/data/calibration/gt_trajectories/straight_line/'
+    directory_1 = os.getcwd() + '/data/calibration/gt_trajectories/left_curve/'
     compare_gt_and_results(directory_1, directory_2)
     plt.show()
