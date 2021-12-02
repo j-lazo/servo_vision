635c71af7e306c63c725e72145f6c672a31a6f3a
j-lazo <j.org.ls@ciencias.unam.mx>
2021-12-02 11:35:48 +0100

    function added to make inferences and returning mask and center points

--- scripts/computer_vision/call_models.py
+++ scripts/computer_vision/call_models.py
@@ -51,6 +51,38 @@ def detect_lumen(model, innput_frame, output_size=(300, 300)):
     return output_imgage, point_x, point_y
 
 
+def detect_lumen_2(model, innput_frame, output_size=(300, 300)):
+    # get the input size of the network
+    input_layer_shape = model.layers[0].input_shape
+    shape_input = np.shape(input_layer_shape)
+    if shape_input[-1] == 4:
+        input_size_x = input_layer_shape[0][1]
+        input_size_y = input_layer_shape[0][2]
+
+    # reshape the input frame to be compatible with the input of the network
+    reshaped_img = cv2.resize(innput_frame, (input_size_x, input_size_y),
+                              interpolation=cv2.INTER_AREA)
+    # apply blur to the image and normalize the image
+    resized = (cv2.blur(reshaped_img, (7, 7)))/255
+    # make a prediction of the mask
+    mask = cvf.predict_mask(model, resized)
+    # resize the image for a size to show to the user
+    output_imgage = cv2.resize(reshaped_img, output_size, interpolation=cv2.INTER_AREA)
+    w, h, d = np.shape(output_imgage)
+    # calculate the center points of the lumen according to the detected mask
+    point_x, point_y = cvf.detect_dark_region(mask, output_imgage)
+
+    #if not(np.isnan(point_x)):
+    #    cv2.circle(output_imgage, (int(point_x), int(point_y)), 45, (0, 0, 255), 2)
+    #if not(np.isnan(point_y)):
+    #    cv2.circle(output_imgage, (int(point_x), int(point_y)), 25, (0, 0, 255), 2)
+    #    cv2.line(output_imgage, (int(point_x), int(point_y)), (int(w / 2), int(h / 2)), (255, 0, 0), 4)
+    # center of the image
+    #cv2.rectangle(output_imgage, (int(w / 2) - 3, int(h / 2) - 3), (int(w / 2) + 3, int(h / 2) + 3),  (0, 255, 255), -1)
+
+    return output_imgage, mask, point_x, point_y
+
+
 def main():
     cap = cv2.VideoCapture(0)
     project_folder = '/home/nearlab/Jorge/current_work/lumen_segmentation/data/' \
